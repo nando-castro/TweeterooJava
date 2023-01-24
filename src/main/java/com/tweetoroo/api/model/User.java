647b5176af2d0133befad28284fb2eb8c1a0e184
nando-castro <fernando.castro10.master@gmail.com>
2023-01-24 13:28:59 -0300

    feat: implements model and dto user

--- src/main/java/com/tweetoroo/api/model/User.java
+++ src/main/java/com/tweetoroo/api/model/User.java
@@ -2,10 +2,31 @@ package com.tweetoroo.api.model;
 
 import com.tweetoroo.api.dto.UserDTO;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Entity
+@NoArgsConstructor
 public class User {
 
     public User(UserDTO data){
-        
+        this.username = data.username();
+        this.avatar = data.avatar();
     }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String avatar;
     
 }
