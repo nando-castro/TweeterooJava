aa0c9ebd0d0480eb322e39b0fe096a943d0a1db3
nando-castro <fernando.castro10.master@gmail.com>
2023-01-24 17:52:47 -0300

    feat: implements return username

--- src/main/java/com/tweetoroo/api/controller/TweetController.java
+++ src/main/java/com/tweetoroo/api/controller/TweetController.java
@@ -1,11 +1,14 @@
 package com.tweetoroo.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +41,11 @@ public class TweetController {
     @ResponseStatus(code = HttpStatus.OK)
     public Page<Tweet> findAll(@PageableDefault(page = 0, size = 5) @RequestParam String page){
       return service.findAll(page);
+    }
+
+    @GetMapping("{username}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Tweet> findByUsername(@PathVariable String username){
+        return service.findByUsername(username);
     }
 }
--- src/main/java/com/tweetoroo/api/repository/TweetRepository.java
+++ src/main/java/com/tweetoroo/api/repository/TweetRepository.java
@@ -1,5 +1,7 @@
 package com.tweetoroo.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.tweetoroo.api.model.Tweet;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+    public List<Tweet> findByUsername(String username);
     Page<Tweet> findAll(Pageable pageable);
 }
