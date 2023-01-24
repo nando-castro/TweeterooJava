1130a52e5387990495ee86803ea06f2c6bac5c4f
nando-castro <fernando.castro10.master@gmail.com>
2023-01-24 15:54:11 -0300

    feat: implements status code

--- src/main/java/com/tweetoroo/api/controller/TweetController.java
+++ src/main/java/com/tweetoroo/api/controller/TweetController.java
@@ -3,10 +3,13 @@ package com.tweetoroo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweetoroo.api.dto.TweetDTO;
@@ -16,6 +19,7 @@ import com.tweetoroo.api.service.TweetService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/tweets")
 public class TweetController {
     
@@ -23,12 +27,14 @@ public class TweetController {
     private TweetService service;
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public String createTweet(@RequestBody @Valid TweetDTO req){
         service.createTweet(req);
         return "OK";
     }
 
     @GetMapping()
+    @ResponseStatus(code = HttpStatus.OK)
     public List<Tweet> getTweets(){
         return service.findAllTweets();
     }
--- src/main/java/com/tweetoroo/api/controller/UserController.java
+++ src/main/java/com/tweetoroo/api/controller/UserController.java
@@ -2,6 +2,7 @@ package com.tweetoroo.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
